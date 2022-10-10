7aedc27ba81340085b651134562abf2abe29484b
Muhammed Ali <muhammedaliaydin97@gmail.com>
2022-10-10 23:42:17 +0300

    Player movements implemented

--- src/component/Player.js
+++ src/component/Player.js
@@ -4,8 +4,11 @@ import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
 import { useKeyboard } from "../hooks/useKeyboard";
 
+const JUMP_FORCE = 4;
+const SPEED = 4;
+
 export const Player = () => {
-  const action = useKeyboard();
+  const { moveBackward, moveForward, moveLeft, moveRight, jump } = useKeyboard();
   const { camera } = useThree();
   const [playerRef, playerApi] = useSphere(() => ({
     mass: 1,
@@ -30,6 +33,18 @@ export const Player = () => {
   useFrame(() => {
     // This hook will run on every frame
     camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
+
+    const direction = new Vector3();
+    const frontVector = new Vector3(0, 0, (moveBackward ? 1 : 0) - (moveForward ? 1 : 0));
+    const sideVector = new Vector3((moveLeft ? 1 : 0) - (moveRight ? 1 : 0), 0, 0);
+
+    direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED).applyEuler(camera.rotation);
+
+    playerApi.velocity.set(direction.x, velocity.current[1], direction.z);
+
+    if (jump && Math.abs(velocity.current[1] < 0.05)) {
+      playerApi.velocity.set(velocity.current[0], JUMP_FORCE, velocity.current[2]);
+    }
   });
 
   return <mesh ref={playerRef}></mesh>;
